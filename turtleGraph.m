e15e941794c6512dfa64c73a3063e8f3ce6fbf27
ヾ(≧∇≦*)ゝ <iluvmizuki@gmail.com>
2016-11-17 14:09:03 +0100

    :+1:  :sa: :kimono:

--- turtleGraph.m
+++ turtleGraph.m
@@ -1,33 +1,28 @@
-function turtleCommands = turtleGraph(LindenmayerString)
+function turtleCommands = turtleGraph(LindenmayerString, System ,N)
 
-NumberOfL = sum( LindenmayerString == 'S');
-NumberOfA = sum( LindenmayerString == 'A');
-NumberOfB = sum( LindenmayerString == 'B');
-% An if statement to determine the type of system
+
 turtleCommands=zeros(1, length(LindenmayerString));
 
-if NumberOfL >0 && (NumberOfA== 0 && NumberOfB== 0)
+if strcmpi(System,'koch')
 
-Iteration = (1+log(NumberOfL)/log(4));
-Length = (1/3)^(Iteration-1);
+
+Length = (1/3)^(N);
 
 turtleCommands(LindenmayerString == 'S') = Length;
 turtleCommands(LindenmayerString == 'L') = 2*pi/6;
 turtleCommands(LindenmayerString == 'R') = -4*pi/6;
 
-disp(turtleCommands)
-elseif NumberOfA >0 && NumberOfB > 0 && NumberOfL == 0
+
+elseif strcmpi( 'sierpinski', System)
 
 
-Iteration = input('Iteration no:')
-
-Length = (1/2)^(Iteration-1);
+Length = (1/2)^(N);
 
 turtleCommands(LindenmayerString == 'A') = Length;
 turtleCommands(LindenmayerString == 'B') = Length;
 turtleCommands(LindenmayerString == 'L') = 2*pi/6;
 turtleCommands(LindenmayerString == 'R') = -2*pi/6;
-disp(turtleCommands)
+
 
 else 
 
